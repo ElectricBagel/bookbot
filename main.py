33fe1db2dff927cf95e9e18e716dd1a02ce2f8b2
Sidivan <Sidivan@hotmail.com>
2025-11-13 18:18:56 -0600

    Dictionary with character count

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_book_text
 from stats import book_list
+from stats import text_count
 
 def main():
-
+    
     book_text = get_book_text("books/frankenstein.txt")
     book_count = book_list(book_text)
+    letter_count = text_count(book_text)
     print(f"Found {book_count} total words")
+    print(letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,13 @@ def book_list(booktxt):
     word_count = len(bklist)
     return (word_count)
 
+def text_count(booktxt):
+    char_lower = booktxt.lower()
+    char_list = set(char_lower)
+    char_dict = {}
+    for char in char_list:
+        char_count = char_lower.count(char)
+        char_dict[char] = char_count
+    return char_dict
+
+
