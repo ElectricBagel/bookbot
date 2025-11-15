7993b1055aa7d13a309869bab022597db58a6179
Sidivan <Sidivan@hotmail.com>
2025-11-15 16:52:00 -0600

    Sort and Filter for alpha

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from stats import get_book_text
 from stats import book_list
 from stats import text_count
+from stats import char_sort
 
 def main():
     
     book_text = get_book_text("books/frankenstein.txt")
     book_count = book_list(book_text)
     letter_count = text_count(book_text)
+    sorted_list = char_sort(letter_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {book_count} total words")
-    print(letter_count)
-
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        ch = item["char"]
+        if ch.isalpha() and ch in "abcdefghijklmnopqrstuvwxyz":
+            print(f"{ch}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -17,4 +17,20 @@ def text_count(booktxt):
         char_dict[char] = char_count
     return char_dict
 
+def _by_num(item):
+    return item["num"]
 
+def char_sort(char_2_sort):
+    sort_on = []
+    for c,n in char_2_sort.items():
+        char_dicts = {
+            "char": c,
+            "num": n
+                      }
+        sort_on.append(char_dicts)
+    #print(sort_on[:3])
+    sort_on.sort(reverse=True, key=_by_num)
+   
+    return sort_on
+    #print(f"Sorted Chars: {sorted_char}")
+ #sorted_char = sorted(char_2_sort.keys())
